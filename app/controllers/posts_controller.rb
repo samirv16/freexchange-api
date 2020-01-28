219ec77b73d6d1ac8989445b7973cfeb9a4a6c5d
samirv16 <samir.vallecillo@outlook.com>
2020-01-28 15:02:05 -0600

    installs pry

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,25 +6,26 @@ class PostsController < ApplicationController
     end
 
     def show
-        @post = Post.find(params:[:id])
+        @post = Post.find(params[:id])
 
         render json: @post, status: 200
     end
 
     def create
+        binding.pry
         @post = Post.create(post_params)
 
         render json: @post, status: 200
     end
 
     def update
-        @post = Post.find(params:[:id])
+        @post = Post.find(params[:id])
         @post = Post.update(post_params)
         render json: @post, status: 200
     end
 
     def destroy
-        @post = Post.find(params:[:id])
+        @post = Post.find(params[:id])
         @post.delete
 
         render json: {postID: @post.id}
