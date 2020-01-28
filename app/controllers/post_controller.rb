bd6562da3eb08a77a55460c55a32d5c4f8795f29
samirv16 <samir.vallecillo@outlook.com>
2020-01-27 20:10:44 -0600

    creates post functions

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,38 @@
 class PostController < ApplicationController
+    def index
+        @posts = Post.all 
+
+        render json: @posts, status: 200
+    end
+
+    def show
+        @post = Post.find(params:[:id])
+
+        render json: @post, status: 200
+    end
+
+    def create
+        @post = Post.create(post_params)
+
+        render json: @post, status: 200
+    end
+
+    def update
+        @post = Post.find(params:[:id])
+        @post = Post.update(post_params)
+        render json: @post, status: 200
+    end
+
+    def destroy
+        @post = Post.find(params:[:id])
+        @post.delete
+
+        render json: {postID: @post.id}
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:name)
+    end
 end
