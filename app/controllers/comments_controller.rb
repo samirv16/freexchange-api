750ffc69a43738dcb874564a31fc2482c20197d8
samirv16 <samir.vallecillo@outlook.com>
2020-01-29 00:28:43 -0600

    creats comments controller and functions

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,36 @@
+class CommentsController < ApplicationController
+    def index
+        comments = Comment.all
+        render json: comments, status: 200
+    end
+    def show
+        comment = Comment.find(params[:id])
+        render json: comment, status: 200
+    end 
+
+    def create
+        
+        comment = Comment.new(comment_params)
+        comment.save
+        render json: comment, status: 404
+        
+    end 
+
+    def update
+        comment = Comment.find(params[:id])
+        comment.update(comment_params)
+        render json: comment, status: 200
+    end 
+
+    def destroy
+        comment = Comment.find(params[:id])
+        comment.delete
+        render json: { message: "there goes your comment"}
+    end
+
+    private 
+    
+    def comment_params
+        params.require(:comment).permit(:body, :post_id)
+    end 
+end
