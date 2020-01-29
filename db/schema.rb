55fd3c1af79485436f5997cc3894cd07c4e7185c
samirv16 <samir.vallecillo@outlook.com>
2020-01-29 00:22:55 -0600

    creates comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_28_023753) do
+ActiveRecord::Schema.define(version: 2020_01_29_062200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.string "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "item1"
