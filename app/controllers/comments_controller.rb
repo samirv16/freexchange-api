565378a13d456b4eab663870cc648d607e64a0dd
samirv16 <samir.vallecillo@outlook.com>
2020-01-30 14:36:02 -0600

    deletes test comments from database

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,7 +11,7 @@ class CommentsController < ApplicationController
     def create
         comment = Comment.create(comment_params)
     
-        render json: comment, status: 404
+        render json: comment, status: 200
     end 
 
     def update
